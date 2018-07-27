c1f129c7d4e94b4850478f7fa8299dad25924fb2
Hampton Moore <me@hampton.pw>
2018-07-27 11:00:17 +0200

    WebServer

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // jshint esversion: 6
 // jscs:disable maximumLineLength
+
+const express = require('express')
+const app = express()
 const getter = require('pixel-getter');
 
 function componentToHex(c) {
@@ -13,51 +16,62 @@ function rgbToHex(r, g, b) {
 
 let lastColor = [0, 0, 0];
 
-getter.get(process.argv[2] || 'test.png', (err, pixels) => {
-  let output = '';
-  let size = Math.sqrt(pixels[0].length);
+app.get('/', function(req, res){res.send('Hello World!')});
 
-  let rowCounter = 0;
-
-  output += `${size}z`;
-
-  for (let i = 0; i < pixels[0].length; i += 1) {
-
-    let pixel = [0, 0];
-
-    if (pixels[0][i].a == 0) {
-      pixel[0] = 0;
-    } else {
-      pixel[0] = 1;
-    }
-
-    if (pixel[0] == 0) {
-      output += '1';
-    }
-
-    if (pixel[0] == 1) {
-      if (lastColor[0] != pixels[0][i].r || lastColor[1] != pixels[0][i].g || lastColor[2] != pixels[0][i].b) {
-
-        const color = '';
-
-        output += '5';
-        output += rgbToHex(pixels[0][i].r, pixels[0][i].g, pixels[0][i].b);
-        lastColor = [pixels[0][i].r, pixels[0][i].g, pixels[0][i].b];
-
+app.get('*', function(req, res){
+  try {
+  getter.get(req.originalUrl.substring(1), (err, pixels) => {
+    try {
+    let output = '';
+    let size = Math.sqrt(pixels[0].length);
+  
+    let rowCounter = 0;
+  
+    output += `${size}z`;
+  
+    for (let i = 0; i < pixels[0].length; i += 1) {
+  
+      let pixel = [0, 0];
+  
+      if (pixels[0][i].a == 0) {
+        pixel[0] = 0;
+      } else {
+        pixel[0] = 1;
       }
-
-      output += '2';
+  
+      if (pixel[0] == 0) {
+        output += '1';
+      }
+  
+      if (pixel[0] == 1) {
+        if (lastColor[0] != pixels[0][i].r || lastColor[1] != pixels[0][i].g || lastColor[2] != pixels[0][i].b) {
+  
+          const color = '';
+  
+          output += '5';
+          output += rgbToHex(pixels[0][i].r, pixels[0][i].g, pixels[0][i].b);
+          lastColor = [pixels[0][i].r, pixels[0][i].g, pixels[0][i].b];
+  
+        }
+  
+        output += '2';
+      }
+  
+      rowCounter += 1;
+  
+      if (rowCounter >= size) {
+        output += '0';
+        rowCounter = 0;
+      }
+  
     }
+  
+    res.send(output);
+  } catch(err){}
+  });
 
-    rowCounter += 1;
-
-    if (rowCounter >= size) {
-      output += '0';
-      rowCounter = 0;
-    }
-
-  }
-
-  console.log(output);
-
+} catch(err){}
+  
 });
+
+app.listen(process.env.PORT || 8080, () => console.log('Example app listening on port 8080!'))
